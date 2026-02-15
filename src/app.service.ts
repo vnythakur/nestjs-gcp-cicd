e75893a0b50cfc22c31072329756b30eba1803ef
Vinay Thakur <thakur.vny92@gmail.com>
2026-02-15 12:58:35 +0530

    Test 4

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello() {
     return {
-      message: 'Hello World - 3 from ' + (process.env.DATABASE_USER ?? 'No Database User')
+      message: 'Hello World - 4 from ' + (process.env.DATABASE_USER ?? 'No Database User')
     };
   }
 }
